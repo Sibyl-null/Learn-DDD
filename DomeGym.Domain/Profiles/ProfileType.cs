1bb1fbe1a00ec981efdf405a7a922cd2a0bc560d
Sibyl-null <1286438810@qq.com>
2024-10-26 23:39:28 +0800

    [feat]: ProfileType 定义

---
+++ DomeGym.Domain/Profiles/ProfileType.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.SmartEnum;
+
+namespace DomeGym.Domain.Profiles;
+
+public class ProfileType : SmartEnum<ProfileType>
+{
+    public static readonly ProfileType Admin = new(nameof(Admin), 0);
+    public static readonly ProfileType Trainer = new(nameof(Trainer), 1);
+    public static readonly ProfileType Participant = new(nameof(Participant), 2);
+    
+    private ProfileType(string name, int value) : base(name, value)
+    {
+    }
+}
