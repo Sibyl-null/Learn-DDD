693a5a2d64502ba1fa827182386acb02046feb07
Sibyl-null <1286438810@qq.com>
2024-10-26 17:55:49 +0800

    [feat]: TDD - Participant 不变量实现

---
+++ DomeGym.Domain.UnitTest/ParticipantTests.cs
@@ -0,0 +1,42 @@
+﻿using DomeGym.Domain.UnitTest.TestUtils.Common;
+using DomeGym.Domain.UnitTest.TestUtils.Participants;
+using DomeGym.Domain.UnitTest.TestUtils.Sessions;
+using ErrorOr;
+using FluentAssertions;
+
+namespace DomeGym.Domain.UnitTest;
+
+public class ParticipantTests
+{
+    [Theory]
+    [InlineData(1, 3, 1, 3)]
+    [InlineData(1, 3, 2, 3)]
+    [InlineData(1, 3, 2, 4)]
+    [InlineData(1, 3, 0, 2)]
+    public void AddSessionToSchedule_WhenSessionOverlapsWithAnotherSession_ShouldFail(
+        int startHourSession1,
+        int endHourSession1,
+        int startHourSession2,
+        int endHourSession2)
+    {
+        // Arrange
+        var participant = ParticipantFactory.CreateParticipant();
+        
+        var session1 = SessionFactory.CreateSession(
+            id: Guid.NewGuid(),
+            time: TimeRangeFactory.CreateFromHours(startHourSession1, endHourSession1));
+        
+        var session2 = SessionFactory.CreateSession(
+            id: Guid.NewGuid(),
+            time: TimeRangeFactory.CreateFromHours(startHourSession2, endHourSession2));
+        
+        // Act
+        ErrorOr<Success> result1 = participant.AddSessionToSchedule(session1);
+        ErrorOr<Success> result2 = participant.AddSessionToSchedule(session2);
+
+        // Assert
+        result1.IsError.Should().BeFalse();
+        result2.IsError.Should().BeTrue();
+        result2.FirstError.Should().Be(ParticipantErrors.CannotHaveTwoOrMoreOverlappingSessions);
+    }
+}
--- DomeGym.Domain/Participant.cs
+++ DomeGym.Domain/Participant.cs
@@ -1,8 +1,11 @@
+using ErrorOr;
+
 namespace DomeGym.Domain;
 
 public class Participant
 {
     private readonly Guid _userId;
+    private readonly Schedule _schedule = Schedule.Empty();
     private readonly List<Guid> _sessionIds = new();
 
     public Guid Id { get; }
@@ -11,5 +14,18 @@ public class Participant
     {
         Id = id ?? Guid.NewGuid();
         _userId = userId;
+    }
+
+    public ErrorOr<Success> AddSessionToSchedule(Session session)
+    {
+        if (_sessionIds.Contains(session.Id))
+            return Error.Conflict($"Session {session.Id} already added to schedule");
+
+        ErrorOr<Success> boolResult = _schedule.BookTimeSlot(session.Date, session.Time);
+        if (boolResult is { IsError: true, FirstError.Type: ErrorType.Conflict })
+            return ParticipantErrors.CannotHaveTwoOrMoreOverlappingSessions;
+
+        _sessionIds.Add(session.Id);
+        return Result.Success;
     }
 }
