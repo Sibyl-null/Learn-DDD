8fe941be2f5d85a6f187000c7087175051809e1e
Sibyl-null <1286438810@qq.com>
2024-10-26 15:41:50 +0800

    [feat]: TDD - Trainer 不变量实现

---
+++ DomeGym.Domain.UnitTest/TestUtils/Trainers/TrainerFactory.cs
@@ -0,0 +1,11 @@
+﻿using DomeGym.Domain.UnitTest.TestConstants;
+
+namespace DomeGym.Domain.UnitTest.TestUtils.Trainers;
+
+public static class TrainerFactory
+{
+    public static Trainer CreateTrainer(Guid? id = null)
+    {
+        return new Trainer(id: id ?? Constants.Trainer.Id);
+    }
+}
--- DomeGym.Domain/Trainer.cs
+++ DomeGym.Domain/Trainer.cs
@@ -1,8 +1,30 @@
+using ErrorOr;
+
 namespace DomeGym.Domain;
 
 public class Trainer
 {
     private readonly Guid _id;
     private readonly Guid _userId;
-    private readonly List<Guid> _sessionIds;
+    private readonly Schedule _schedule;
+    private readonly List<Guid> _sessionIds = new();
+
+    public Trainer(Guid? id, Schedule? schedule = null)
+    {
+        _schedule = schedule ?? Schedule.Empty();
+        _id = id ?? Guid.NewGuid();
+    }
+
+    public ErrorOr<Success> AddSessionToSchedule(Session session)
+    {
+        if (_sessionIds.Contains(session.Id))
+            return Error.Conflict($"Session {session.Id} already added to schedule");
+
+        ErrorOr<Success> boolResult = _schedule.BookTimeSlot(session.Date, session.Time);
+        if (boolResult is { IsError: true, FirstError.Type: ErrorType.Conflict })
+            return TrainerErrors.CannotHaveTwoOrMoreOverlappingSessions;
+
+        _sessionIds.Add(session.Id);
+        return Result.Success;
+    }
 }
